162174b432df9c1caca180317041d1416e633130
BurleVineeth <burle.vineeth@daffodilsw.com>
2026-01-12 20:07:50 +0530

    Mobile number validator.

--- src/app/shared/components/login/login.ts
+++ src/app/shared/components/login/login.ts
@@ -1,15 +1,20 @@
 import { MobileFormat } from '@/shared/directives/mobile-format';
 import { Component, output } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Button } from '../button/button';
 
 @Component({
   selector: 'app-login',
-  imports: [Button, MobileFormat],
+  imports: [Button, MobileFormat, ReactiveFormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
 export class Login {
   public close = output();
+
+  public form = new FormGroup({
+    mobile: new FormControl('', [Validators.required]),
+  });
 
   public onBack() {
     this.close.emit();
--- src/app/shared/directives/mobile-format.ts
+++ src/app/shared/directives/mobile-format.ts
@@ -1,22 +1,26 @@
-import { Directive, ElementRef, HostListener } from '@angular/core';
+import { Directive, ElementRef, HostListener, forwardRef } from '@angular/core';
+import { AbstractControl, NG_VALIDATORS, ValidationErrors, Validator } from '@angular/forms';
 
 @Directive({
   selector: '[appMobileFormat]',
+  providers: [
+    {
+      provide: NG_VALIDATORS,
+      useExisting: forwardRef(() => MobileFormat),
+      multi: true,
+    },
+  ],
 })
-export class MobileFormat {
+export class MobileFormat implements Validator {
   constructor(private el: ElementRef<HTMLInputElement>) {}
 
-  @HostListener('input', ['$event'])
-  onInput(event: Event) {
+  @HostListener('input')
+  onInput() {
     const input = this.el.nativeElement;
 
-    // Remove non-digits
     let value = input.value.replace(/\D/g, '');
-
-    // Limit to 10 digits
     value = value.slice(0, 10);
 
-    // Add space after 5 digits
     if (value.length > 5) {
       value = value.slice(0, 5) + ' ' + value.slice(5);
     }
@@ -24,10 +28,16 @@ export class MobileFormat {
     input.value = value;
   }
 
-  @HostListener('keydown', ['$event'])
-  onKeyDown(event: KeyboardEvent) {
-    if (['e', 'E', '+', '-', '.'].includes(event.key)) {
-      event.preventDefault();
+  validate(control: AbstractControl): ValidationErrors | null {
+    if (!control.value) return null;
+
+    const rawValue = control.value.replace(/\s/g, '');
+    const mobileRegex = /^[6-9]\d{9}$/;
+
+    if (!mobileRegex.test(rawValue)) {
+      return { invalidMobile: true };
     }
+
+    return null;
   }
 }
